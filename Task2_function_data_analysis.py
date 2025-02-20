c30ec0ed138a496bc9300acb6422dc9f1d8ac156
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-02-20 09:09:06 +0100

    added comments

--- Task2_function_data_analysis.py
+++ Task2_function_data_analysis.py
@@ -1,20 +1,23 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #Importing maplotlib
 
 def data_file_analysz(file_path):
     dna_sequences = {}
     current_id = None
 
+    # Open the file and process it line by line
     with open(file_path, "r") as file:
         for line in file:
-            line = line.strip().lower()
+            line = line.strip().lower() #cleaning the string and converts to lower case
 
+            #creates key without ">" 
             if line.startswith(">"):
                 current_id = line[1:]
-                dna_sequences[current_id] = ""
-            
+                dna_sequences[current_id] = "" #Creates empty value
+
             else:
-                dna_sequences[current_id] += line
-      
+                dna_sequences[current_id] += line #append data as values
+
+    # Count the frequency of each letter (a, t, c, g) in every sequence 
     all_letter_count= {}
 
     for seq_id, sequence in dna_sequences.items():
@@ -28,7 +31,7 @@ def data_file_analysz(file_path):
     return all_letter_count
     
 def plot_data(dna_data):
-    
+    #Plot the bar chart for each DNA sequences   
     for seq, count in dna_data.items():
         plt.figure(figsize=(6,4))
         plt.bar(list(count.keys()), list(count.values()), color=['blue', 'red', 'green', 'purple'])
